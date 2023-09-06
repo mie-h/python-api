003a2ab219003d35a17bbf59288d0b6cf674c050
mie-h <miehaga@berkeley.edu>
2023-09-05 18:58:03 -0700

    test to image with dreamstudio

---
+++ text2img.py
@@ -0,0 +1,29 @@
+import base64
+import os
+import requests
+
+engine_id = "stable-diffusion-xl-1024-v1-0"
+api_host = os.getenv("API_HOST", "https://api.stability.ai")
+api_key = os.getenv("STABILITY_API_KEY")
+
+if api_key is None:
+    raise Exception("Missing Stability API key.")
+
+response = requests.post(
+    f"{api_host}/v1/generation/{engine_id}/text-to-image",
+    headers={
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": f"Bearer {api_key}",
+    },
+    json={"text_prompts": [{"text": "A flying dog"}]},
+)
+
+if response.status_code != 200:
+    raise Exception("Non-200 response: " + str(response.text))
+
+data = response.json()
+
+for i, image in enumerate(data["artifacts"]):
+    with open(f"v1_txt2img_{i}.png", "wb") as f:
+        f.write(base64.b64decode(image["base64"]))
