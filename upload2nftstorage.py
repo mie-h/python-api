38147d0aa3b38ec1d686da5627061574cc5e2e51
mie-h <miehaga@berkeley.edu>
2023-09-07 09:19:51 -0700

    script to upload img to nft storage

--- upload2nftstorage.py
+++ upload2nftstorage.py
@@ -1,5 +1,23 @@
-import requests
+import os
+import nft_storage
+from nft_storage.api import nft_storage_api
+from nft_storage.model.error_response import ErrorResponse
+from nft_storage.model.upload_response import UploadResponse
+from nft_storage.model.unauthorized_error_response import UnauthorizedErrorResponse
+from nft_storage.model.forbidden_error_response import ForbiddenErrorResponse
 
-url = "http://file.api.wechat.com/cgi-bin/media/upload?access_token=ACCESS_TOKEN&type=TYPE"
-files = {"media": open("test.jpg", "rb")}
-requests.post(url, files=files)
+
+# Configure Bearer authorization (JWT): bearerAuth
+configuration = nft_storage.Configuration(access_token=os.getenv("NFT_STORAGE_API_KEY"))
+
+# Enter a context with an instance of the API client
+with nft_storage.ApiClient(configuration) as api_client:
+    # Create an instance of the API class
+    api_instance = nft_storage_api.NFTStorageAPI(api_client)
+
+    body = open(f"v1_txt2img_0.png", "rb")
+    try:
+        api_response = api_instance.store(body, _check_return_type=False)
+        print(api_response)
+    except nft_storage.ApiException as e:
+        print("Exception when calling NFTStorageAPI->store: %s\n" % e)
